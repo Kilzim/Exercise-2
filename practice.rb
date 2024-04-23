5d22edcd3a9a23fc20215b87270b85552ad749a9
Kilian Zimmermann <kilian.zimmermann@kellogg.northwestern.edu>
2024-04-23 20:16:03 +0000

    23.04.

--- practice.rb
+++ practice.rb
@@ -28,22 +28,63 @@ blockchain = [
 
 # 👇👇👇 Your code HERE 👇👇👇
 
+# We'll use a hash to keep track of each user's balance
+balance = {}
 
-def calculate_balance(blockchain, user)
-  total = 0
-  blockchain.each do |transaction|
-    if transaction["from_user"] == user
-      total -= transaction["amount"]
-    elsif transaction["to_user"] == user
-      total += transaction["amount"]
-    end
-  end
-  total
+# Loop through each transaction in the blockchain
+blockchain.each do |transaction|
+  # Get the from_user and to_user for the current transaction
+  from_user = transaction["from_user"]
+  to_user = transaction["to_user"]
+
+  # Get the amount of KelloggCoin being transferred
+  amount = transaction["amount"]
+
+  # If the from_user doesn't have a balance yet, initialize it to 0
+  balance[from_user] ||= 0
+  # Subtract the amount from the from_user's balance
+  balance[from_user] -= amount
+
+  # If the to_user doesn't have a balance yet, initialize it to 0
+  balance[to_user] ||= 0
+  # Add the amount to the to_user's balance
+  balance[to_user] += amount
 end
 
+# Now we can print out each user's balance
+puts "Ben's KelloggCoin balance is #{balance['ben']}"
+puts "Brian's KelloggCoin balance is #{balance['brian']}"
+puts "Evan's KelloggCoin balance is #{balance['evan']}"
+puts "Anthony's KelloggCoin balance is #{balance['anthony']}"
 
-puts "Ben's KelloggCoin balance is #{calculate_balance(blockchain, "ben")}"
-puts "Brian's KelloggCoin balance is #{calculate_balance(blockchain, "brian")}"
-puts "Evan's KelloggCoin balance is #{calculate_balance(blockchain, "evan")}"
-puts "Anthony's KelloggCoin balance is #{calculate_balance(blockchain, "anthony")}"
+
+# balances = {}
+
+# blockchain.each do |transaction|
+#   balances[transaction["to_user"]] = (balances[transaction["to_user"]].to_i + transaction["amount"])
+#   balances[transaction["from_user"]] = (balances[transaction["from_user"]].to_i - transaction["amount"]) if transaction["from_user"]
+# end
+
+# balances.each do |user, balance|
+#   puts "#{user}'s KelloggCoin balance is #{balance}"
+# end
+# __________________________________________________________________________
+
+# def calculate_balance(blockchain, user)
+#   total = 0
+#   blockchain.each do |transaction|
+#     if transaction["from_user"] == user
+#       total -= transaction["amount"]
+#     elsif transaction["to_user"] == user
+#       total += transaction["amount"]
+#     end
+#   end
+#   total
+# end
+
+
+# puts "Ben's KelloggCoin balance is #{calculate_balance(blockchain, "ben")}"
+# puts "Brian's KelloggCoin balance is #{calculate_balance(blockchain, "brian")}"
+# puts "Evan's KelloggCoin balance is #{calculate_balance(blockchain, "evan")}"
+# puts "Anthony's KelloggCoin balance is #{calculate_balance(blockchain, "anthony")}"
 
